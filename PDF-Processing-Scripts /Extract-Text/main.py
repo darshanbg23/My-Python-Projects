e3106761158d20bb84dc094bb6f4ab05be2dc962
Darshan BG <darshan.bg.23@gmail.com>
2025-02-13 16:46:58 +0530

    Update main.py

--- PDF-Processing-Scripts /Extract-Text/main.py
+++ PDF-Processing-Scripts /Extract-Text/main.py
@@ -1,1 +1,8 @@
+import PyPDF2  
 
+with open("sample.pdf", "rb") as pdf_file:
+    reader = PyPDF2.PdfReader(pdf_file) 
+    
+    for page_num in range(len(reader.pages)):  
+        page = reader.pages[page_num]
+        print(f"Text from Page {page_num + 1}:\n{page.extract_text()}\n") 
